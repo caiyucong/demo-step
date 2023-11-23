2a0eafc54cc30ac1add43961f710343f8638202a
caiyucong <caiyucong12@gmail.com>
2023-11-23 09:10:44 +0800

    changes

--- src/components/NewComponent.vue
+++ src/components/NewComponent.vue
@@ -1,4 +1,11 @@
 <template>
-  <div>这是一个崭新的页面！</div>
+  <div class="main">这是一个崭新的页面！</div>
 </template>
 <script setup lang="ts"></script>
+<style>
+.main {
+  width: 400px;
+  height: 400px;
+  line-height: 400px;
+}
+</style>
